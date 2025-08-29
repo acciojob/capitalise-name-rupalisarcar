e73f86e5eda98fe8ead8cdfbd53e2dd65a322303
rupalisarcar <rupalisarcar@gmail.com>
2025-08-29 23:47:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function changeUpperCase(){
+	const personName= document.getElementById("fname");
+	//console.log(personName)
+	if(personName){
+		personName.value = personName.value.toUpperCase()
+	}
+}
+changeUpperCase();
